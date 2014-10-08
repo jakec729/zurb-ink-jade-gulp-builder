38c01ad548e9b12a19b79e3f366fecc9ca807a35
Jake Cooper <jakec729@mac.com>
2014-10-07 22:57:46 -0400

    updated gulpfile with installed packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,73 @@
+/* Plugins */
+/* -------------------------------------- */
+
 var gulp = require('gulp'),
-	jade = require('gulp-jade');
+	jade = require('gulp-jade'),
+	sass = require('gulp-ruby-sass'),
+	rename = require('gulp-rename'),
+	minify = require('gulp-minify-css'),
+	prefixer = require('gulp-autoprefixer'),
+	inlineCSS = require('gulp-inline-css');
 
 
-gulp.task('templates', function() {
+/* Processing Tasks */
+/* -------------------------------------- */
+
+gulp.task('styles', function(){
+
+	gulp.src('sass/*.scss')
+
+		// Compile Sass
+		.pipe(sass({
+			style: "expanded",
+			precision: 6,
+			noCache: true
+		}))
+
+		// Save 'Pretty' version
+		.pipe(gulp.dest('css'))
+
+		// Prefix the CSS
+		.pipe(prefixer({
+			browsers: ['last 2 versions'],
+            cascade: false
+		}))
+
+		// Minify the CSS
+		.pipe(minify())
+
+		// Change filename for dist file
+		.pipe(rename({ suffix: "-min" }))
+
+		// Save 'Dist' version
+		.pipe(gulp.dest('css'))
+
+});
+
+gulp.task('templates', ['styles'], function() {
 	var YOUR_LOCALS = {};
 
-	gulp.src('./src/*.jade')
+	gulp.src('src/*.jade')
+
+		// Process Jade files
 		.pipe(jade({
 			pretty: true
 		}))
-		.pipe(gulp.dest('./public/'))
+
+		// Inline the CSS
+		.pipe(inlineCSS({}))
+
+		// Export to public folder
+		.pipe(gulp.dest('public'))
 });
 
+gulp.task('default', ['templates', 'watch'] );
 
-gulp.task('default', ['templates', 'watch'], function(){
 
-});
+/* Watch Tasks */
+/* -------------------------------------- */
 
-gulp.task('watch', function(){
-
-	gulp.watch('src/*.jade',['templates']);
-
+gulp.task('watch', function() {
+	gulp.watch('src/*.jade', ['templates']);
+	gulp.watch('sass/**/*.scss', ['styles']);
 });
